e288ea58ba39315934ed1503eef28910b702ec4e
Gizmozi <110856454+Gizmozi@users.noreply.github.com>
2023-08-01 15:08:44 +0200

    Add files via upload

---
+++ js/rool.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", windowLoad);
+function windowLoad() {
+  document.addEventListener("click", documentActions);
+}
+function documentActions(e) {
+  const targetElement = e.target;
+
+  if (targetElement.hasAttribute("data-goto")) {
+    const gotoElement = document.querySelector("${targetElement.dataset.goto}");
+    const headerHeight = document.querySelector(".header").offsetHeight;
+    if (gotoElement) {
+      window.scrollTo({
+        top: gotoElement.offsetTop - headerHeight,
+        behavior: "smooth",
+      });
+    }
+    e.preventDefault();
+  }
+}
